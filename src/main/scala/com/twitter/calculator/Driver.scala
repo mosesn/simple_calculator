5e092d0277abb98da8f249de1caea21f219d872c
Moses Nakamura <mnakamura@twitter.com>
2013-11-22 14:22:26 -0500

    pretty and foldlefting

--- src/main/scala/com/twitter/calculator/Driver.scala
+++ src/main/scala/com/twitter/calculator/Driver.scala
@@ -3,35 +3,28 @@ package com.twitter.calculator
 import java.util.Stack
 
 object Driver {
-  def main(args: Array[String]) {
-    println(evaluate(args))
-  }
-
-  def stackify(args: Array[String]): List[Int] = {
-    val eithers = args map parseArg
-    recursiveStackify(eithers, Nil)
-  }
-
   type Token = Either[Int, (Int, Int) => Int]
 
-  def recursiveStackify(args: Array[Token], stack: List[Int]): List[Int] = {
-    if (args.isEmpty)
-      stack
-    else {
-      val stk = args.head match {
-        case Left(num) => num :: stack
-        case Right(operator) =>
-          operator(stack.head, stack.tail.head) :: stack.tail.tail
-      }
-      recursiveStackify(args.tail, stk)
+  def main(args: Array[String]) {
+    println(evaluate(args.toList))
+  }
+
+  def stackify(args: List[String]): List[Int] = {
+    val eithers = args map parseArg
+    eithers.foldLeft[List[Int]](Nil)(process)
+  }
+
+  def process(stack: List[Int], token: Token): List[Int] = token match {
+    case Left(num) => num :: stack
+    case Right(operator) => stack match {
+      case x :: y :: tail => operator(x, y) :: tail
+      case _ => throw new Exception("explode")
     }
   }
 
-  def evaluate(args: Array[String]): Int = {
-    stackify(args) match {
-      case x :: Nil => x
-      case _ => throw new Exception("stack did not have one element")
-    }
+  def evaluate(args: List[String]): Int = stackify(args) match {
+    case x :: Nil => x
+    case _ => throw new Exception("stack did not have one element")
   }
 
   def parseArg(arg: String): Token = arg match {
